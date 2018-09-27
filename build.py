6395955fe5ed2c03191a4b718ae76458f1e32d98
Efraim Gentil <efraim.gentil@gmail.com>
2018-09-26 22:35:12 -0300

    Adding build process through the python script

--- build.py
+++ build.py
@@ -1,11 +1,15 @@
 import os
 from distutils import dir_util
 
-
-#my_git = "git@github.com:efraimgentil/efraimgentil.github.io.git";
 my_git = "https://github.com/efraimgentil/efraimgentil.github.io.git"
 
-os.system( "jekyll build")
+current_path = os.getcwd()
+
+print("# Building docker image for building process")
+os.system("docker build . -t blog-builder")
+
+print("## Running build process")
+os.system("docker run -v " + current_path + ":/app blog-builder jekyll build")
 
 print("### CREATING THE COMMIT DIR ###")
 os.system("mkdir tocommit")
@@ -25,6 +29,6 @@ os.system( "git commit -am 'Update blog'")
 
 os.system( "git push origin master")
 
-print("### LIMPANDO PASTA TOCOMMIT ###")
+print("#### LIMPANDO PASTA TOCOMMIT ###")
 os.chdir("..")
 os.system("rm -rf tocommit")
